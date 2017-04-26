2a284965ebecd75232eaf8afaf4820ee1932f3da
Gregory Fabry <gfabry@westmonroepartners.com>
2017-04-26 16:06:29 -0500

    restructuring, tweaks

--- WMP.Alexa/DefaultAlexaController.cs
+++ WMP.Alexa/DefaultAlexaController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -11,7 +12,37 @@ namespace WMP.Alexa
     {
         public IHttpActionResult Post(HttpRequestMessage httpRequest)
         {
-            return Ok((new T()).HandleHttpRequest(httpRequest));
+            AlexaRequest request = JsonConvert.DeserializeObject<AlexaRequest>(httpRequest.Content.ReadAsStringAsync().Result);
+            byte[] alexaBytes = httpRequest.Content.ReadAsByteArrayAsync().Result;
+
+            DateTime now = DateTime.Now; // reference time for this request
+
+            string chainUrl = null;
+            if (!httpRequest.Headers.Contains("SignatureCertChainUrl") || String.IsNullOrEmpty(chainUrl = httpRequest.Headers.GetValues("SignatureCertChainUrl").First()))
+            {
+                throw new UnauthorizedAccessException();
+            }
+
+            string signature = null;
+            if (!httpRequest.Headers.Contains("Signature") || String.IsNullOrEmpty(signature = httpRequest.Headers.GetValues("Signature").First()))
+            {
+                throw new UnauthorizedAccessException();
+            }
+
+            if (!SpeechletRequestSignatureVerifier.VerifyRequestSignature(alexaBytes, signature, chainUrl))
+                throw new UnauthorizedAccessException();
+
+            DateTime parsed = DateTime.Parse(request.Body.Timestamp);
+
+            if ((now - parsed).TotalSeconds > 150)
+                throw new UnauthorizedAccessException();
+
+            AlexaResponse response = new AlexaResponse();
+            response.SessionAttributes = request.Session.Attributes;
+
+            (new T()).HandleRequest(response.SessionAttributes, request, response);
+
+            return Ok(response);
         }
     }
 }
---
+++ WMP.Alexa/Example/DateFinderSkill.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WMP.Alexa;
+using System.Web.Http;
+
+namespace AlexaSkills.Controllers
+{
+    [Route("dateskill")]
+    public class DateController : DefaultAlexaController<DateFinderSkill> { }
+
+    public class DateFinderSkill : AlexaSkill
+    {
+        public override void StartSession(AlexaSession session, AlexaRequest request, AlexaResponse response)
+        {
+            response.SayText("If you tell me a date, I can tell you what day it was. What date would you like to know?");
+        }
+
+        public override void EndSession(AlexaSession session, AlexaRequest request, AlexaResponse response)
+        {
+            // No need to do anything
+        }
+
+        [Intent("GetDate")]
+        public void GetDate(AlexaSession session, AlexaRequest request, AlexaResponse response)
+        {
+            string dateString = request.Body.Intent.Slots["Date"].Value;
+
+            if (dateString == null || dateString == String.Empty || dateString.Split('-').Length != 3)
+            {
+                response.SayText("I didn't get that. Tell me a date and I'll tell you what day of the week it represents.");
+                return;
+            }
+
+            string[] rawDate = dateString.Split('-');
+
+            DateTime date = new DateTime(Convert.ToInt32(rawDate[0]), Convert.ToInt32(rawDate[1]), Convert.ToInt32(rawDate[2]));
+
+            string interText = " is a ";
+            if (date < DateTime.Today)
+                interText = " was on a ";
+            else if (date > DateTime.Today)
+                interText = " will be on a ";
+
+
+            response.SayText(dateString + interText + date.DayOfWeek);
+            response.EndSession();
+        }
+    }
+}
